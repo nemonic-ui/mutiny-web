2297a50ec3adc3fa02ca7cc15272283eee7317e5
info <info@technicaledge.org>
2024-12-04 12:29:11 +0000

    Tested load safe mode.

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+
+{ pkgs }: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    #pkgs.nodePackages.vite  # Add vite as a package
+  ];
+  idx.extensions = [
+    "svelte.svelte-vscode"
+    "vue.volar"
+  ];
+
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npx"              # Use npx to run vite
+          "vite"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
